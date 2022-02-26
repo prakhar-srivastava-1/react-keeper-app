e35c52de3bffbf9b6c5a1b5ae9ba719dc62d32a8
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-26 15:23:26 +0530

    add Header.jsx component

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//3. Create a Header.jsx component that renders a <header> element
+//to show the Keeper App name in an <h1>.
+function Header() {
+  return <h1>Keeper App</h1>
+}
+
+export default Header;
