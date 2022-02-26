4d50e5dc6fa12a77d2adc2c5866462954cc2daba
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-26 16:05:01 +0530

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//4. Create a Footer.jsx component that renders a <footer> element
+//to show a copyright message in a <p> with a dynamically updated year.
+function Footer() {
+  return (
+    <footer>
+      <p>
+        &copy; Copyright {new Date().getFullYear()}.
+      </p>
+    </footer>
+  );
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ import App from "./components/App.jsx";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
-//4. Create a Footer.jsx component that renders a <footer> element
-//to show a copyright message in a <p> with a dynamically updated year.
+
 //5. Create a Note.jsx component to show a <div> element with a
 //<h1> for a title and a <p> for the content.
 //6. Make sure that the final website is styled like the example shown here:
