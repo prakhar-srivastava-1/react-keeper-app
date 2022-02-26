06ade7078570ce37776a175aa26c2d9d27d4d343
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-26 16:30:55 +0530

    add Note component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 // import Header component into App
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
 //2. Create a App.jsx component.
 function App() {
   return (
     <div>
       <Header />
+      <Note />
       <Footer />
     </div>
   );
---
+++ src/components/Note.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+//5. Create a Note.jsx component to show a <div> element with a
+//<h1> for a title and a <p> for the content.
+function Note() {
+  return (
+    <div>
+      <h1>
+        Dummy Note
+      </h1>
+      <p>
+        This is a dummy note! We will try to reuse this component for other notes.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
