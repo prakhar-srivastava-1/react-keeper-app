d1ccfab8414ad9a20276ec0dbbc0c830e227f2b9
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-02-27 08:21:31 +0530

    import notes from notes.js in App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,18 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+
+// import notes from notes.js
+import notes from "../notes.js";
+
 //2. Create a App.jsx component.
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {/*Fetch notes using an Arrow function*/}
+      {<Note />}
+
       <Footer />
     </div>
   );
