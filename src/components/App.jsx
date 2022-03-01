989ae5ac647f2c9a2dc25e51ea67fc5873533256
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-03-01 06:44:46 +0530

    add CreateArea component and use in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-// import Header component into App
-import Header from "./Header.jsx";
-import Footer from "./Footer.jsx";
-import Note from "./Note.jsx";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
-// import notes from notes.js
-import notes from "../notes.js";
-
-//2. Create a App.jsx component.
 function App() {
   return (
     <div>
       <Header />
-      {/*Fetch notes using an Arrow function*/}
-      {notes.map(note => <Note key={note.key} title={note.title} content={note.content} />)}
-
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
