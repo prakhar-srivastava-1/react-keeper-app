dd06ff1c8eac730e8bfb0507e08284e53b62127a
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-03-01 09:09:38 +0530

    fix bug - stop form submission
    To do this, form attribute onSubmit has been changed to stop default
    behaviour. A better way would have been to use onClick function only
    however, on removing form tag, styling would break. So the other option
    was to re-style the page.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -17,12 +17,12 @@ function CreateArea(props) {
 
   return (
     <div>
-      <form>
+      <form onSubmit={event => event.preventDefault()}>
         <input onChange={captureNote} name="title" placeholder="Title" value={title} />
         <textarea onChange={captureNote} name="content" placeholder="Take a note..." rows="3" value={content} />
-        <button onClick={() => {
+        <button onClick={(event) => {
           console.log({title, content});
-          props.createNewNote({title, content});
+          props.createNewNote({title, content}, event);
         }}>
           Add
         </button>
