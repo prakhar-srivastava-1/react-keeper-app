00bf8e3f41b22b3ceacec455a1010f073e747991
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-03-02 07:58:16 +0530

    add DEL button to each note and logic

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,6 +3,12 @@ import React from "react";
 //5. Create a Note.jsx component to show a <div> element with a
 //<h1> for a title and a <p> for the content.
 function Note(props) {
+
+  function handleClick(itemId) {
+    console.log(itemId);
+    props.deleteNote(itemId);
+  }
+
   return (
     <div className="note">
       <h1>
@@ -11,6 +17,9 @@ function Note(props) {
       <p>
         {props.content}
       </p>
+      <button onClick={
+        () => handleClick(props.id)
+      }>DEL</button>
     </div>
   );
 }
