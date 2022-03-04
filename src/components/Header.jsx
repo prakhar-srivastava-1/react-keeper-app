9c3df8820baee96ab5587f2cf7e23d0ae2489646
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-03-04 21:52:33 +0530

    add HighlightIcon component as app icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import HighlightIcon from '@material-ui/icons/Highlight';
 
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
 function Header() {
   return (
     <header>
-      <h1>Keeper App</h1>
+      <h1><HighlightIcon /> Keeper App</h1>
     </header>
   );
 }
