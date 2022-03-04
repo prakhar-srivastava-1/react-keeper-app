828a328d7c9dee7e67f0543aa7f34c661ff006a9
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-03-04 21:21:58 +0530

    make ADD button zoom in
    To make this transition effect work, wrap the Fab button inside Zoom
    component. It is also a part of material-ui/core.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Add from '@material-ui/icons/Add';
 import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
 
@@ -22,12 +23,14 @@ function CreateArea(props) {
       <form onSubmit={event => event.preventDefault()} className="create-note">
         <input onChange={captureNote} name="title" placeholder="Title" value={title} />
         <textarea onChange={captureNote} name="content" placeholder="Take a note..." rows="3" value={content} />
-        <Fab onClick={(event) => {
-          props.createNewNote({title, content}, event);
-          setNote({title: "", content: ""});
-        }}>
-          <Add />
-        </Fab>
+        <Zoom in={true}>
+          <Fab onClick={(event) => {
+            props.createNewNote({title, content}, event);
+            setNote({title: "", content: ""});
+          }}>
+            <Add />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
